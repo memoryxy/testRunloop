65de41f23f3098f068afdb1ff7d67fb9a2fbccfc
wangjianfei <hzwangjianfei@corp.netease.com>
2015-11-17 10:43:13 +0800

    add 4

---
+++ testRunloop/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  testRunloop
+//
+//  Created by wangjianfei on 15/11/17.
+//  Copyright © 2015年 Sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
---
+++ testRunloop/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  testRunloop
+//
+//  Created by wangjianfei on 15/11/17.
+//  Copyright © 2015年 Sina. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
